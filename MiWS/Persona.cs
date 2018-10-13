b477ed68894eeb4462af4e82e778205aa9d542f5
Carlos Andrés Romero Parrado <andrestrue@hotmail.com>
2018-10-13 12:04:37 -0500

    INICIAL

---
+++ MiWS/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace MiWS
+{
+    [DataContract]
+    public class Persona: BaseRespuesta
+    {
+        [DataMember]
+        public string Nombre { get; set; }
+        [DataMember]
+        public int Edad { get; set; }
+        public string Token { get; set; }
+    }
+
+    public class BaseRespuesta
+    {
+        public int Error { get; set; }
+        public string Mensaje { get; set; }
+        public BaseRespuesta()
+        {
+            Error = 1;
+        }
+    }
+}
